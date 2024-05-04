25b579fde43d1474e019472a667713391eb7f9e7
Varun Prajapati <varunprajapati123@gmail.com>
2024-05-04 17:35:53 +0530

    added methods in user and video models

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,6 @@
 import mongoose, {Schema} from "mongoose";
-
+import jwt from "jsonwebtoken";
+import bcrupt from "bcrypt";
 
 const userSchema = new Schema({
     username: {
@@ -17,7 +18,7 @@ const userSchema = new Schema({
         lowercase: true,
         trim: true,
     },
-    fullname: {
+    fullName: {
         type: String,
         required: true,
         trim: true,
@@ -46,5 +47,42 @@ const userSchema = new Schema({
 
 }, {timestamps: true});
 
+
+
+userSchema.pre("save", async function (next) {
+    if(!this.isModified("password")) return next();
+    this.password = bcrupt.hash(this.password, 10);
+    this();
+})
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrupt.compare(password, this.password);
+}
+
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullName: this.fullName,
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+};
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+};
 
 export const User = mongoose.model("User", userSchema);
